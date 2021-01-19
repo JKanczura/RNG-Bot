89bc1e020a9d6765c2c97a9fe166b3ff87f5c043
jkanczura <jessicakanczura@gmail.com>
2021-01-18 21:28:36 -0500

    wrong str meth

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.startwith('$hello'):
+    if message.content.startswith('$hello'):
         await message.channel.send('Thrussy!')
 
 client.run(os.getenv('TOKEN'))
