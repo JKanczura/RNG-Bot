98677c756de33e9caee9218783a997d97515e2ef
jkanczura <jessicakanczura@gmail.com>
2021-01-18 21:39:12 -0500

    add randomizer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import random
 
 client = discord.Client()
 
@@ -12,7 +13,11 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.startswith('$hello'):
-        await message.channel.send('Thrussy!')
+    if message.content.startswith('$roll'):
+        splitRet = message.content.split(' ', 0)
+        s = int(splitRet[1], 10)
+        random.seed(s)
+
+        await message.channel.send(random.Random())
 
 client.run(os.getenv('TOKEN'))
